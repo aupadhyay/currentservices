091a2eca3e5152195c6de124eeb9acb821024a4c
sidarth-raman <sidarth_raman@brown.edu>
2024-02-05 21:17:00 -0800

    feat: click for next slide

--- pages/[project].tsx
+++ pages/[project].tsx
@@ -1,8 +1,30 @@
 import { getProjectByName, getProjects } from "@/api";
 import Layout from "@/components/Layout";
 import { GetStaticPaths, GetStaticProps } from "next";
+import router from "next/router";
+import React, { useState, useEffect } from 'react';
+
+
+const SlideNumber = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  useEffect(() => {
+    const handleClick = () => {
+      setSlideNumber((prevCount) => prevCount + 1);
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []); 
+
+  return slideNumber;
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  
   const projects = await getProjects();
   //@ts-ignore
   const paths = projects.map((project) => ({
@@ -19,28 +41,37 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return { props: { project } };
 };
 
+const handleClick = () => {
+  // Handle the click event, such as logging or performing other actions
+  console.log("Link clicked!");
+  router.push("/");
+};
+
+
+
 const Header = () => (
   <div className="flex justify-between">
     <div>
-      <h1 className="text-xl text-white">Current Services</h1>
+      <h1 className="text-xl text-white" onClick={handleClick}>Current Services</h1>
     </div>
     <div>
-      <h1 className="text-xl text-white">Index</h1>
+      <h1 className="text-xl text-white" onClick={handleClick}>Index</h1>
     </div>
   </div>
 );
 
+
 //@ts-ignore
 const ProjectPage = ({ project }) => {
+  const slideNumber = SlideNumber();
   return (
     <div>
-      {project.attributes.slides.map((slide) => (
-        <Layout color="none" top={<Header />} key={slide.title}>
+        <Layout color="none" top={<Header />} key={project.attributes.slides[slideNumber].title}>
           <div className="px-24 py-20">
-            <p className="text-white text-2xl w-3/4">{slide.description}</p>
+            <p className="text-white text-2xl w-3/4">{project.attributes.slides[slideNumber].description}</p>
+            {"Slide:" + slideNumber}
           </div>
         </Layout>
-      ))}
     </div>
   );
 };
