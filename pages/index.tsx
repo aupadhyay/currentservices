310871d3756a105f80855072e8866db3582ec9c6
Abhi Upadhyay <abhi.upadhyay@berkeley.edu>
2024-04-14 19:54:05 -0700

    fix: faster hover load + index spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export const Index = ({ projects }: { projects: IProject[] }) => {
   const indexTabs = (
     <div
       className={clsx(
-        "font-favorit font-book text-[32px] flex sm:flex-row flex-col-reverse gap-5 transition-colors ease-in-out duration-500",
+        "font-favorit font-book text-[32px] flex sm:flex-row flex-col-reverse transition-colors ease-in-out duration-500",
         `text-${textColor}`
       )}
     >
@@ -89,12 +89,18 @@ export const Index = ({ projects }: { projects: IProject[] }) => {
           }}
           onMouseLeave={() => setSelected(projects[0])}
           className={clsx(
-            "select-none text-xl hover:border-b-2 border-current transition-colors ease-in-out duration-500 w-fit cursor-[inherit]",
-            `text-${textColor}`,
-            selected.slug === project.slug && "border-b-2 border-current"
+            "select-none text-xl w-fit cursor-[inherit] px-3",
+            `text-${textColor}`
           )}
         >
-          {project.title}
+          <span
+            className={clsx(
+              "w-fit hover:border-b-2 border-current transition-colors ease-in-out duration-500",
+              selected.slug === project.slug && "border-b-2 border-current"
+            )}
+          >
+            {project.title}
+          </span>
         </Link>
       ))}
     </div>
@@ -104,19 +110,27 @@ export const Index = ({ projects }: { projects: IProject[] }) => {
     <Header color={selected.indexTextColor} showIndex={true} />
   )
 
+  const videos = (selected: IProject) => {
+    return projects.map((project) => (
+      <video
+        key={project.slug}
+        autoPlay
+        muted
+        loop
+        className="absolute top-1/2 left-1/2 min-w-full min-h-full max-w-none"
+        style={{
+          transform: "translate(-50%, -50%)",
+          zIndex: selected.slug === project.slug ? 1 : 0,
+        }}
+        src={BASE_URL + project.hoverVideo.data.attributes.url}
+      />
+    ))
+  }
+
   return (
-    <Layout top={header(selected)} bottom={indexTabs} cursor='angled'>
+    <Layout top={header(selected)} bottom={indexTabs} cursor="angled">
       <div className="w-full h-full relative overflow-hidden">
-        <video
-          autoPlay
-          muted
-          loop
-          className="absolute top-1/2 left-1/2 min-w-full min-h-full max-w-none"
-          style={{
-            transform: "translate(-50%, -50%)",
-          }}
-          src={BASE_URL + selected.hoverVideo.data.attributes.url}
-        />
+        {videos(selected)}
       </div>
     </Layout>
   )
