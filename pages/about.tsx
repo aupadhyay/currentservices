811d0710ed3d81b264b455c29be7ebb98ad46389
sidarth-raman <sidarth_raman@brown.edu>
2024-06-21 14:29:39 -0700

    fix: fades on red pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -47,8 +47,8 @@ export default function About() {
       }
       about={true}
     >
-      <div className="px-10 sm:px-20 lg:px-[16.66%] py-32 flex flex-col justify-center h-full">
-        <p className="hidden sm:block text-white text-2xl w-full lg:w-5/6">
+      <div className="px-10 sm:px-20 lg:px-[16.66%] py-32 flex flex-col justify-center h-full animate-fadeIn">
+        <p className="hidden sm:block text-white text-2xl w-full lg:w-5/6 ">
           Current Services is an interdisciplinary design and strategy practice
           founded in 2020. Operating as a core group that often extends into a
           broader network, we employ an array of methodologies to support every
@@ -80,81 +80,78 @@ export default function About() {
           &nbsp;our offices.
         </p>
 
-        {/*Desktop*/}
-        <div className="hidden sm:block">
-          <div className="grid grid-cols-1 sm:grid-cols-4 mt-14">
-            {[...Array(numColumns)].map((_, columnIndex) => (
-              <div className="col-span-1" key={columnIndex}>
-                {serviceTitles
-                  .slice(
-                    columnIndex * wordsPerTitleList,
-                    (columnIndex + 1) * wordsPerTitleList
-                  )
-                  .map((word, index) => (
-                    <p
-                      className="text-white text-[20px] sm:text-xl mt-1"
-                      key={index}
-                    >
-                      {word}
-                    </p>
-                  ))}
-              </div>
-            ))}
+        <div className="animate-fadeIn">
+          {/* Desktop */}
+          <div className="hidden sm:block">
+            <div className="grid grid-cols-1 sm:grid-cols-4 mt-14">
+              {[...Array(numColumns)].map((_, columnIndex) => (
+                <div className="col-span-1" key={columnIndex}>
+                  {serviceTitles
+                    .slice(
+                      columnIndex * wordsPerTitleList,
+                      (columnIndex + 1) * wordsPerTitleList
+                    )
+                    .map((word, index) => (
+                      <p
+                        className="text-white text-[20px] sm:text-xl mt-1"
+                        key={index}
+                      >
+                        {word}
+                      </p>
+                    ))}
+                </div>
+              ))}
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-4 mt-6">
+              {[...Array(numColumns)].map((_, columnIndex) => (
+                <div className="col-span-1" key={columnIndex}>
+                  {services
+                    .slice(
+                      columnIndex * wordsPerList,
+                      (columnIndex + 1) * wordsPerList
+                    )
+                    .map((word, index) => (
+                      <p
+                        className="text-white text-[20px] lg:text-xl mt-1"
+                        key={index}
+                      >
+                        {word}
+                      </p>
+                    ))}
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-4 mt-6">
-            {[...Array(numColumns)].map((_, columnIndex) => (
-              <div className="col-span-1" key={columnIndex}>
-                {services
-                  .slice(
-                    columnIndex * wordsPerList,
-                    (columnIndex + 1) * wordsPerList
-                  )
-                  .map((word, index) => (
-                    <p
-                      className="text-white text-[20px] lg:text-xl mt-1"
-                      key={index}
-                    >
-                      {word}
-                    </p>
-                  ))}
-              </div>
-            ))}
-          </div>
-        </div>
-        {/*Mobile*/}
-        <div className="block sm:hidden">
-          <div className="grid grid-cols-1 sm:grid-cols-4 mt-16 sm:mt-0">
-            {[...Array(numColumns)].map((_, columnIndex) => (
-              <div className="col-span-1" key={columnIndex}>
-                {serviceTitles
-                  .slice(
-                    columnIndex * wordsPerTitleList,
-                    (columnIndex + 1) * wordsPerTitleList
-                  )
-                  .map((word, index) => (
-                    <p
-                      className="text-white text-[21px] mt-1 sm:mt-0"
-                      key={index}
-                    >
-                      {word}
-                    </p>
-                  ))}
-                {services
-                  .slice(
-                    columnIndex * wordsPerList,
-                    (columnIndex + 1) * wordsPerList
-                  )
-                  .map((word, index) => (
-                    <p
-                      className="text-white text-[21px] pl-20 mt-1 sm:mt-0"
-                      key={index}
-                    >
-                      {word}
-                    </p>
-                  ))}
-                <br />
-              </div>
-            ))}
+
+          {/* Mobile */}
+          <div className="block sm:hidden mt-16">
+            <div className="grid grid-cols-1 sm:grid-cols-4">
+              {[...Array(numColumns)].map((_, columnIndex) => (
+                <div className="col-span-1" key={columnIndex}>
+                  {serviceTitles
+                    .slice(
+                      columnIndex * wordsPerTitleList,
+                      (columnIndex + 1) * wordsPerTitleList
+                    )
+                    .map((word, index) => (
+                      <p className="text-white text-[21px] mt-1" key={index}>
+                        {word}
+                      </p>
+                    ))}
+                  {services
+                    .slice(
+                      columnIndex * wordsPerList,
+                      (columnIndex + 1) * wordsPerList
+                    )
+                    .map((word, index) => (
+                      <p className="text-white text-[21px] pl-20 mt-1" key={index}>
+                        {word}
+                      </p>
+                    ))}
+                  <br />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
